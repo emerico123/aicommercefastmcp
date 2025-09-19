93bd562fce259ad2b6b550143f1cab9af2480489
emerico123 <abishek@emerico.biz>
2025-09-19 16:59:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,3 @@ mcp = FastMCP("Echo Server")
 def echo_tool(text: str) -> str:
     """Echo the input text"""
     return text
-
-
-@mcp.resource("echo://static")
-def echo_resource() -> str:
-    return "Echo!"
-
-
-@mcp.resource("echo://{text}")
-def echo_template(text: str) -> str:
-    """Echo the input text"""
-    return f"Echo: {text}"
-
-
-@mcp.prompt("echo")
-def echo_prompt(text: str) -> str:
-    return text
